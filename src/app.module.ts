3221c2268c0bbcc35e959f6a13f63104eb7374c8
Ryan Smith <mrryantsmith@gmail.com>
2022-01-28 06:52:04 -0600

    Remove cat references from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './cats/cats.module';
 import { SurveysModule } from './surveys/surveys.module';
 
 @Module({
@@ -13,7 +12,6 @@ import { SurveysModule } from './surveys/surveys.module';
       isGlobal: true
     }),
     MongooseModule.forRoot(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }),
-    CatsModule,
     SurveysModule
   ],
   controllers: [AppController],
