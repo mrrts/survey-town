3074b8571ab777726196660ccfbee24c9f9cf7e8
Ryan Smith <mrryantsmith@gmail.com>
2022-02-10 08:00:25 -0600

    try setting trust proxy to resolve https cookie issue

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.set('trust proxy', 1);
   app.use(
     session({
       name: process.env.SESSION_COOKIE_NAME,
@@ -14,6 +16,7 @@ async function bootstrap() {
       saveUninitialized: false,
       cookie: {
         secure: process.env.USE_SECURE_SESSION,
+        httpOnly: true,
         maxAge: /* 2 weeks in millis */ 1209600000,
       },
     } as session.SessionOptions),
