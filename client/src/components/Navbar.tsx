696491f26757245d983070ba7d32d4054156d901
Ryan Smith <mrryantsmith@gmail.com>
2022-02-11 08:11:26 -0600

    make logged-in-user a navbar dropdown element

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,11 +3,12 @@ import BSNavbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPoll } from '@fortawesome/free-solid-svg-icons';
+import { faPoll, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { Link, navigate } from '@reach/router';
 import { useAppDispatch, useAppSelector } from '../store';
 import { logoutUser } from '../store/auth/slice';
 import { getUser } from '../store/auth/selectors';
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 export interface INavbarProps {
 }
@@ -30,19 +31,30 @@ export const Navbar: FC<INavbarProps> = () => {
               <Nav.Link as={Link} to='/surveys'>Surveys</Nav.Link>
             </Nav>
             <BSNavbar.Collapse className="justify-content-end">
-              <BSNavbar.Text>
                 {user && (
                   <>
-                    {user.handle} | &nbsp;
-                    <button className='btn-link logout-button' onClick={() => dispatch(logoutUser())}>
-                      Logout
-                    </button>
+                    <NavDropdown 
+                      title={
+                        <>
+                          {user.handle}
+                          <FontAwesomeIcon className='user-icon' icon={faUserCircle} />
+                        </>
+                      }
+                    >
+                      <NavDropdown.Item
+                        as='button'
+                        onClick={() => dispatch(logoutUser())}
+                      >
+                        Logout
+                      </NavDropdown.Item>
+                    </NavDropdown>
                   </>
                 )}
                 {!user && (
-                  <Link className='btn btn-link' to='/login'>Login</Link>
+                  <BSNavbar.Text>
+                    <Link className='btn btn-link' to='/login'>Login</Link>
+                  </BSNavbar.Text>
                 )}
-              </BSNavbar.Text>
             </BSNavbar.Collapse>
           </BSNavbar.Collapse>
         </Container>
