8fc83501910a9e097f74680abbcbec8ac1b2ff5b
Ryan Smith <mrryantsmith@gmail.com>
2022-02-12 06:50:17 -0600

    remove delayed render from create/edit survey modal

--- client/src/components/surveys/SurveyGeneralFormModal.tsx
+++ client/src/components/surveys/SurveyGeneralFormModal.tsx
@@ -13,8 +13,6 @@ import { CreateSurveyDto } from '../../entities/dtos/create-survey.dto';
 import { createSurvey, updateSurvey } from '../../store/surveys/slice';
 import { useSurvey } from '../../util/hooks/useSurvey.hook';
 import { UpdateSurveyDto } from '../../entities/dtos/update-survey.dto';
-import { useDelayedRender } from '../../util/hooks/useDelayedRender.hook';
-import { Spinner } from '../common/Spinner';
 
 export interface ISurveyGeneralFormModalProps {
 
@@ -23,10 +21,9 @@ export interface ISurveyGeneralFormModalProps {
 export const SurveyGeneralFormModal: FC<ISurveyGeneralFormModalProps> = () => {
   const modal = useModal(ModalKeys.SURVEY_GENERAL);
   const surveyId = modal.data?.surveyId;
-  
   const { survey } = useSurvey(surveyId);
-  const showForm = useDelayedRender(1000, [modal.isOpen, surveyId]).show;
   const dispatch = useAppDispatch();
+
   const schema = yup.object().shape({
     title: yup.string().required().min(4).max(100),
     description: yup.string().required().min(4).max(500)
@@ -67,10 +64,10 @@ export const SurveyGeneralFormModal: FC<ISurveyGeneralFormModalProps> = () => {
   };
 
   useEffect(() => {
-    if (showForm) {
+    if (modal.isOpen) {
       reset();
     }
-  }, [showForm, reset]);
+  }, [modal.isOpen, reset]);
 
   if (!modal.isOpen) {
     return null;
@@ -83,32 +80,27 @@ export const SurveyGeneralFormModal: FC<ISurveyGeneralFormModalProps> = () => {
           <Modal.Title>{survey ? 'Edit' : 'New'} Survey</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {!showForm && <Spinner />}
-          {showForm && (
-            <>
-              <Form.Group>
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  { ...register('title') }
-                  type='text'
-                  maxLength={100}
-                  defaultValue={survey?.title}
-                ></Form.Control>
-                <Form.Text className='text-danger'>{errors.title?.message}</Form.Text>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Description</Form.Label>
-                <Form.Control
-                  { ...register('description') }
-                  as='textarea'
-                  maxLength={500}
-                  rows={2}
-                  defaultValue={survey?.description}
-                ></Form.Control>
-                <Form.Text className='text-danger'>{errors.description?.message}</Form.Text>
-              </Form.Group>
-            </>
-          )}
+          <Form.Group>
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              { ...register('title') }
+              type='text'
+              maxLength={100}
+              defaultValue={survey?.title}
+            ></Form.Control>
+            <Form.Text className='text-danger'>{errors.title?.message}</Form.Text>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              { ...register('description') }
+              as='textarea'
+              maxLength={500}
+              rows={2}
+              defaultValue={survey?.description}
+            ></Form.Control>
+            <Form.Text className='text-danger'>{errors.description?.message}</Form.Text>
+          </Form.Group>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseClick}>
